41419ed1e7e6eeb82e00fd8d718a07a3ee33ed8e
lzh8639-max <lzh8639-max@users.noreply.github.com>
2026-06-26 04:49:15 +0000

    deploy: 040182c6d6d8e0ad953f6407f9129d04a3197968

---
+++ _astro/url-utils.DPzxBSG0.js
@@ -0,0 +1,1 @@
+import"./translation.Db_rEr8H.js";import"./config.BHFVRzy5.js";function a(r,e){return r.replace(/^\/|\/$/g,"").toLowerCase()===e.replace(/^\/|\/$/g,"").toLowerCase()}function o(...r){return r.join("/").replace(/\/+/g,"/")}function i(r){return o("","/mizuki-blog/",r)}export{i as n,a as t};
